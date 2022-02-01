177f26e80b50b89bb34e25369788f57229752726
Oskar Taubert <oskar.taubert@kit.edu>
2022-02-01 18:00:04 +0100

    sketched out model

--- model.py
+++ model.py
@@ -25,3 +25,46 @@ class LoadForecaster(nn.Module):
         cell_state = torch.randn(self.num_layers, batch_size, self.hidden_size, device=self.device)
 
         return hidden_state, cell_state
+
+
+class NaiveModel(nn.Module):
+    def __init__(self, training_data):
+        self.holidays = holidays.CountryHoliday('DE', prov=None, state='NI')
+        # TODO learn weights and values here
+        self.weeks = torch.empty((53, 24,), dtype=float)
+        self.seasons = torch.empty((7, 24,), dtype=float)
+        self.holidays = torch.empty((2, 24,), dtype=float)
+        w1 = None
+        w2 = None
+        w3 = None
+
+    def forward(self, metadata):
+        d = datetime.date(*metadata.split('-'))
+        _, week, day = d.isocalendar()
+        holiday = metadata in self.holidays
+
+        return self.w1 * self.weeks[day] + self.w2 * self.seasons[weeks] + self.w3 * sekf,holiday[int(holiday)]
+
+
+class SophisticatedModel(LightningModule):
+
+    def __init__(self, naive_model):
+        self.naive_model = naive_model
+        # self.transformer = nn.Transformer(d_model = 64,
+        #         n_head = 4,
+        #         num_encoder_layers = 2,
+        #         num_decoder_layers = 6,
+        #         dim_feedworward = 128,
+        #         dropout=0.1,)
+
+
+    def forward(self, x):
+        return self.naive_model(x)
+
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        prediction = self(x)
+
+        lossval = self.criterion(prediction, y)
+        return lossval
